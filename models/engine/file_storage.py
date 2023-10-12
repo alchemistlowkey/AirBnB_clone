8548ac77730c3aa31addc0c97c9942adf1dcdf97
alchemistlowkey <alchemistlowkey@gmail.com>
2023-10-12 11:56:27 -0700

    file storage started

---
+++ models/engine/file_storage.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+File Storage class
+"""
+
+
+class FileStorage:
+    """
+    A class that serializes/deserializes instances to a JSON file viz-a-vis
+    """
+    __file_path = "file.json"
+    __objects = {}
