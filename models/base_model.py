4b8b7be3fdbb875a08348d7a4dc6b6f6c4f0e6b5
alchemistlowkey <alchemistlowkey@gmail.com>
2023-10-12 14:13:57 -0700

    file storage done

--- models/base_model.py
+++ models/base_model.py
@@ -5,6 +5,7 @@ The Base model for the AirBnB
 
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
@@ -36,6 +37,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """
@@ -55,6 +57,7 @@ class BaseModel:
         """
 
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """
@@ -67,22 +70,3 @@ class BaseModel:
         dicto["created_at"] = self.created_at.isoformat()
         dicto["updated_at"] = self.updated_at.isoformat()
         return dicto
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,6 +2,7 @@
 """
 File Storage class
 """
+import json
 
 
 class FileStorage:
@@ -10,3 +11,49 @@ class FileStorage:
     """
     __file_path = "file.json"
     __objects = {}
+
+    def all(self):
+        """
+        Returns the dictionary __objects
+        """
+
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """
+        Sets in __objects the obj with key <obj class name>.id
+        """
+
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        FileStorage.__objects[key] = obj
+
+    def save(self):
+        """
+        Serializes __objects to the JSON file (path: __file_path)
+        """
+
+        serial_json = {}
+        for key, value in FileStorage.__objects.items():
+            serial_json[key] = value.to_dict()
+        with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
+            json.dump(serial_json, f)
+
+    def reload(self):
+        """
+        Deserializes the JSON file to __objects
+        """
+
+        from models.base_model import BaseModel
+
+        class_names = {
+                "BaseModel": BaseModel,
+                }
+
+        try:
+            with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
+                load_file = json.load(f)
+            for key, value in load_file.items():
+                name = value["__class__"]
+                FileStorage.__objects[key] = class_names[name](**value)
+        except:
+            pass
